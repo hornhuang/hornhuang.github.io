7cab561d015f02a004b39393696740aa983b6aab
hornhuang <hornhuang@tencent.com>
2021-08-24 16:27:10 +0800

    fix:{路由}
    fix:{路由}

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "a578d61ec96d758075d61695ee6b2693",
-"index.html": "c6e9d7d6eac28a013fb7293fc3bc3728",
-"/": "c6e9d7d6eac28a013fb7293fc3bc3728",
-"main.dart.js": "c12a30756860371c4372d5f8b981647a",
+"index.html": "5079472f083ec33f22077e271d388eac",
+"/": "5079472f083ec33f22077e271d388eac",
+"main.dart.js": "67a30462ddecc965f7da1ce26b3b8b97",
 "favicon.png": "000fed6a584939f9eb8de435df347ee7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
